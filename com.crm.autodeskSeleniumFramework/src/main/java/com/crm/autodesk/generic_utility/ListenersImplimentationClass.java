4a399ddeb93d901a98eeba22dfea179f83c3557b
Prathith Acchu <Prathith Acchu@DESKTOP-5KLMRA1>
2022-03-29 12:59:50 +0530

    commit2 push Listeners

--- com.crm.autodeskSeleniumFramework/src/main/java/com/crm/autodesk/generic_utility/ListenersImplimentationClass.java
+++ com.crm.autodeskSeleniumFramework/src/main/java/com/crm/autodesk/generic_utility/ListenersImplimentationClass.java
@@ -4,37 +4,45 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 /**
  * this class is used for Executing the listeners 
  * @author Prathith Acchu
  *
  */
 public class ListenersImplimentationClass implements  ITestListener {
-
+ 
+	ExtentTest test;
+	ExtentReports report;
 	public void onTestStart(ITestResult result) {
-		String Name = result.getMethod().getMethodName();
-		System.out.println(Name+"=========>Test scripts starts");
+	test =report.createTest(result.getMethod().getMethodName());
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		String Name = result.getMethod().getMethodName();
-		System.out.println(Name+"============>Test scripts execution is successfull");
+	test.log(Status.PASS, result.getMethod().getMethodName());
 	}
 
 	public void onTestFailure(ITestResult result) {
-		String Name = result.getMethod().getMethodName();
+		test.log(Status.FAIL, result.getMethod().getMethodName());
+		test.log(Status.FAIL, result.getThrowable());
+		String path=null;
 		try {
-			String path = new WebdriverUtility().toTakeScreenshot(BaseClass.monitorDriver, Name);
+			path = new WebdriverUtility().toTakeScreenshot(BaseClass.monitorDriver, result.getMethod().getMethodName());
 			System.out.println(path);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+		test.addScreenCaptureFromPath(path);
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		String Name = result.getMethod().getMethodName();
-		System.out.println(Name+"===>test scripts skipped");
-		System.out.println(result.getThrowable());
+		test.log(Status.SKIP, result.getMethod().getMethodName());
+		test.log(Status.SKIP, result.getThrowable());
 
 	}
 
@@ -47,11 +55,23 @@ public class ListenersImplimentationClass implements  ITestListener {
 	}
 
 	public void onStart(ITestContext context) {
-		System.out.println("suit starts ");
+		/* Step 1:Extent report configuration*/
+		ExtentSparkReporter htmlReporter=new ExtentSparkReporter("./extentReport"+new JavaUtility().getsystemDateandTime());
+		htmlReporter.config().setReportName("Regression execution report");
+		htmlReporter.config().setDocumentTitle("Comcast Automation execution Report");
+		htmlReporter.config().setTheme(Theme.DARK);
+		
+		/*Step 2: Attach the physical report and do the system configuration*/
+		report=new ExtentReports();
+		report.attachReporter(htmlReporter);
+		report.setSystemInfo("OS", "Windows 10");
+		report.setSystemInfo("Environment", "Testing Environment");
+		report.setSystemInfo("URL", "http://localhost:8888");
+		report.setSystemInfo("Reporter Name"," Prathith");
 	}
 
 	public void onFinish(ITestContext context) {
-		System.out.println("suit finished ");
+	report.flush();
 	}
 
 
