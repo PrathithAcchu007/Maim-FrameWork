db5fcfdee9fab2976a8a7291e762f511ec95674f
Prathith Acchu <Prathith Acchu@DESKTOP-5KLMRA1>
2022-03-30 10:11:22 +0530

    ipath constatnts added

---
+++ com.crm.autodeskSeleniumFramework/src/main/java/com/crm/autodesk/generic_utility/IPathConstant.java
@@ -0,0 +1,15 @@
+package com.crm.autodesk.generic_utility;
+/**
+ * This is used to store all the constants paths and common browser paths
+ * @author Prathith Paul
+ *
+ */
+public interface IPathConstant {
+	String PROPERTY_PATH="./src/main/resources/credentials.properties";
+	String EXCEL_PATH="./src/test/resources/TestScriptsData.xlsx";
+	String DATABASE_PATH="jdbc:mysql://localhost:3306/testYantra";
+	String DATABASE_USERNAME="root";
+	String DATABASE_PASSWORD="root";
+	
+
+}
